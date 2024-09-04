b8810b26a879c5492d65c3f3c8e9e6bef78c6fec
Mani Bharathi M <swatsmani.1110@gmail.com>
2024-09-04 22:33:11 +0530

    4th Sep Updates - Java Programs - Matcher

--- Java_Programming/src/test/java/CollectionsFW/test.java
+++ Java_Programming/src/test/java/CollectionsFW/test.java
@@ -7,6 +7,8 @@ import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingDeque;
 import java.util.concurrent.TimeUnit;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class test {
 	public static final String RESET = "\u001B[0m";
@@ -18,26 +20,46 @@ public class test {
 	public static final String CYAN = "\u001B[36m";
 
 	public static void main(String[] args) {
-		Class<?> clazz = Collections_Framework.class;
-        Method[] methods = clazz.getDeclaredMethods();
-        List<Method> Methods = new ArrayList<Method>();
-        for(Method method : methods) {
-        	if (!method.getName().contains("$")) {
-        		System.out.println(method.getName());
-            }
-        }
-        
-       
-       
+//		Class<?> clazz = Collections_Framework.class;
+//        Method[] methods = clazz.getDeclaredMethods();
+//        List<Method> Methods = new ArrayList<Method>();
+//        for(Method method : methods) {
+//        	if (!method.getName().contains("$")) {
+//        		System.out.println(method.getName());
+//            }
+//        }
+		
+		
+//		Matcher matcher;
+//		Pattern CompiledPattern;
+//		List<String> MailIDs = new ArrayList<String>();
+//		MailIDs.add("rainamani.444@gmail.com");
+//		MailIDs.add("abc123gmail.com");
+//		MailIDs.add("swatsmani.1110@gmail.com");
+//		String ptrn = "@gmail.com";
+//		CompiledPattern = Pattern.compile(ptrn);
+//
+//		for (String x : MailIDs) {
+//			matcher = CompiledPattern.matcher(x);
+//			if(matcher.find()) {
+//				System.out.println(matcher.start() + " : " + matcher.end());
+//			}
+//
+//		}
+		
+	//	(int x) -> {System.err.println();}//
+
 	}
-	
+
 	public void A() {
-		
+
 	}
-public void B() {
-		
+
+	public void B() {
+
 	}
-public void C() {
-	
-}
+
+	public void C() {
+
+	}
 }
