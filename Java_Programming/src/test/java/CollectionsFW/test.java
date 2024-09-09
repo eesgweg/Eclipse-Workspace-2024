73236e93ce0ff56cf0c1e7686b22d47e19364737
Mani Bharathi M <swatsmani.1110@gmail.com>
2024-09-09 22:58:50 +0530

    Java study

--- Java_Programming/src/test/java/CollectionsFW/test.java
+++ Java_Programming/src/test/java/CollectionsFW/test.java
@@ -74,28 +74,43 @@ public class test {
 //		names.stream().filter(n -> n.contains("a")).map(n -> n+"_AAA").forEach(n -> System.out.println(n));
 		
 		List<Integer> li = List.of(1,3,6,2,5,4,2,1);
-		Stream<Integer> stream = li.stream();
-		System.out.println(stream);
-		System.err.println("---- sorted().distinct()---------");
-		stream.sorted().distinct().forEach(System.out::println);
-		System.err.println("---- filter()---------");
-		li.stream().filter(n -> n%2 == 0).forEach(System.out::println);
-		System.err.println("---- peek() and limit(3)---------");
-		li.stream().peek(n ->  System.out.println("Peeking: "+n)).limit(3).forEach(System.out::println);
-		System.err.println("---- skip()---------");
-		li.stream().skip(3).forEach(System.out::println);
-		System.err.println("---- reduce()---------");
+//		Stream<Integer> stream = li.stream();
+//		System.out.println(stream);
+//		System.err.println("---- sorted().distinct()---------");
+//		stream.sorted().distinct().forEach(System.out::println);
+//		System.err.println("---- filter()---------");
+//		li.stream().filter(n -> n%2 == 0).forEach(System.out::print);
+//		 System.out.println();
+//		System.err.println("---- peek() and limit(3)---------");
+//		li.stream().peek(n ->  System.out.println("Peeking: "+n)).limit(3).forEach(System.out::println);
+//        System.err.println("---- skip()---------");
+//		li.stream().skip(3).forEach(System.out::println);
+//		System.err.println("---- reduce()---------");		
+//		int sum = li.stream().reduce(2,(a,b) -> a+b);
+//		System.out.println(sum);		
+//		Set<Integer> Convertedset =li.stream().collect(Collectors.toSet());
+//		System.out.println("Convertedset: "+Convertedset);	
+//		System.err.println(Convertedset.stream().allMatch(n -> n%2 == 0));
+//		System.err.println(Convertedset.stream().anyMatch(n -> n%2 == 1));
+//		System.err.println(Convertedset.stream().noneMatch(n -> n%5 == 0));
+//		System.err.println("------ Map()---------");
+//		Convertedset.stream().map(n -> n*2).forEach(System.out::println);
 		
-		int sum = li.stream().reduce(2,(a,b) -> a+b);
-		System.out.println(sum);
 		
-		Set<Integer> Convertedset =li.stream().collect(Collectors.toSet());
-		System.out.println("Convertedset: "+Convertedset);
+		int[] a = {1,2,3,4,5,6,7};
+		List<Integer> al = new ArrayList<Integer>();
+		for(int x :a) {
+			al.add(x);
+		}
+		System.out.println(al);
+		int k =2;
+//		System.out.println(al.subList(k+1, al.size()));
+//		System.out.println(al.subList(0, k+1));
+		List<Integer> cl = Stream.concat(al.subList(k+1, al.size()).stream(), al.subList(0, k+1).stream()).collect(Collectors.toList());
+		System.out.println(cl);
 		
-		System.out.println(Convertedset.stream().allMatch(n -> n%2 == 0));
-		System.out.println(Convertedset.stream().anyMatch(n -> n%2 == 1));
-		System.out.println(Convertedset.stream().noneMatch(n -> n%5 == 0));
-		Convertedset.stream().map(n -> n*2).forEach(System.out::println);
+		
+		
 		
 		
 	}
